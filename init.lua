3d0d429edec27ecc5324e28c45b641047d906f6a
Sandeep450.Kumar <Sandeep450.Kumar@ril.com>
2024-09-16 11:57:30 +0530

    svelte and disable italics

--- init.lua
+++ init.lua
@@ -545,6 +545,7 @@ require('lazy').setup({
             },
           },
         },
+        svelte = {},
         docker_compose_language_service = {},
         dockerls = {},
         jdtls = {
@@ -903,6 +904,15 @@ require('lazy').setup({
     },
   },
 })
+
+-- disable italics
+local hl_groups = vim.api.nvim_get_hl(0, {})
+
+for key, hl_group in pairs(hl_groups) do
+  if hl_group.italic then
+    vim.api.nvim_set_hl(0, key, vim.tbl_extend('force', hl_group, { italic = false }))
+  end
+end
 
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
