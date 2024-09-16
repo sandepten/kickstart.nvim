bfe45a50ca26011011762540f3f597a096bf9924
Sandeep450.Kumar <Sandeep450.Kumar@ril.com>
2024-09-16 13:58:46 +0530

    catppuccin theme

--- init.lua
+++ init.lua
@@ -782,16 +782,34 @@ require('lazy').setup({
     -- change the command in the config to whatever the name of that colorscheme is.
     --
     -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
-    'folke/tokyonight.nvim',
+    'catppuccin/nvim',
+    name = 'catppuccin',
     priority = 1000, -- Make sure to load this before all the other start plugins.
+    opts = {
+      flavour = 'mocha',
+      transparent_background = true,
+      no_italic = true,
+      default_integrations = true,
+      integrations = {
+        cmp = true,
+        gitsigns = true,
+        nvimtree = true,
+        treesitter = true,
+        notify = true,
+        mini = {
+          enabled = true,
+          indentscope_color = '',
+        },
+      },
+    },
     init = function()
       -- Load the colorscheme here.
       -- Like many other themes, this one has different styles, and you could load
       -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      vim.cmd.colorscheme 'tokyonight-night'
+      vim.cmd.colorscheme 'catppuccin'
 
       -- You can configure highlights by doing something like:
-      vim.cmd.hi 'Comment gui=none'
+      -- vim.cmd.hi 'Comment gui=none'
     end,
   },
 
@@ -904,15 +922,6 @@ require('lazy').setup({
     },
   },
 })
-
--- disable italics
-local hl_groups = vim.api.nvim_get_hl(0, {})
-
-for key, hl_group in pairs(hl_groups) do
-  if hl_group.italic then
-    vim.api.nvim_set_hl(0, key, vim.tbl_extend('force', hl_group, { italic = false }))
-  end
-end
 
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
