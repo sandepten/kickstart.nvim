12afd0b840c20ec3c47f0a8f307f2c2387ccd43f
Sandeep Kumar <sandepten@gmail.com>
2024-09-19 08:10:40 +0530

    add: plugin refactoring

---
+++ lua/kickstart/plugins/theme.lua
@@ -0,0 +1,35 @@
+return { -- You can easily change to a different colorscheme.
+  -- Change the name of the colorscheme plugin below, and then
+  -- change the command in the config to whatever the name of that colorscheme is.
+  --
+  -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
+  'catppuccin/nvim',
+  name = 'catppuccin',
+  priority = 1000, -- Make sure to load this before all the other start plugins.
+  opts = {
+    flavour = 'mocha',
+    transparent_background = true,
+    no_italic = true,
+    default_integrations = true,
+    integrations = {
+      cmp = true,
+      gitsigns = true,
+      nvimtree = true,
+      treesitter = true,
+      notify = true,
+      mini = {
+        enabled = true,
+        indentscope_color = '',
+      },
+    },
+  },
+  init = function()
+    -- Load the colorscheme here.
+    -- Like many other themes, this one has different styles, and you could load
+    -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
+    vim.cmd.colorscheme 'catppuccin'
+
+    -- You can configure highlights by doing something like:
+    -- vim.cmd.hi 'Comment gui=none'
+  end,
+}
